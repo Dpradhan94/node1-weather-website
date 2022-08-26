640e9aaa0e63b0083c87b18b8ffee72413780348
Deba Pradhan <debasispradhan2@gmail.co>
2022-08-26 22:13:10 +0530

    Init commit

---
+++ src/app.js
@@ -0,0 +1,99 @@
+const express = require('express')
+const path = require('path')
+const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+app.set('view engine', 'hbs')
+app.set('views', viewPath)
+hbs.registerPartials(partialsPath)
+
+app.use(express.static(publicDirectoryPath))
+
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weaher',
+        name: 'Debashis'
+    })
+})
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Debashis'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        message: 'Need any help',
+        name: 'Debashis'
+    })
+})
+
+app.get('/weather', (req, res) => {
+    const address = req.query.address
+       if (!req.query.address) {
+       return res.send({
+            error: 'You must provide an address'
+        })
+    } 
+
+        geocode(address, (error, { latitude, longitude, location } = {}) => {
+            if (error) {
+                return res.send({ error }) 
+            } 
+            
+             forecast(latitude, longitude, (error, forecastData) => {
+                if (error) {
+                    return res.send({ error })
+                }
+
+                res.send({
+                    forecast: forecastData,
+                    location,
+                    address: req.query.address
+                })
+            })
+        })
+    
+})
+
+app.get('/products', (req, res) => {
+    if(!req.query.search) {
+       return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+
+    console.log(req.query)
+    res.send({
+        products: []
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Debashis',
+        errorMessage: 'Help artical not found.'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Debashis',
+        errorMessage: 'Page not found.'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'https://api.openweathermap.org/data/2.5/weather?lat='+latitude+'&lon='+longitude+'&units=metric&appid=645038973f42dd29f38acb9fe78c6843'
+
+request({ url, json: true}, (error, { body }) => {
+    if(error) {
+        callback('Unable to connect to Weather Service!', undefined)
+    } else if(body.message) {
+        callback('Unable to find location', undefined)
+    } else {
+        callback(undefined, body.weather[0].main+'. It is currently '+body.main.temp+' degress out. It Feels like '+body.main.feels_like+' degress out.')
+    }
+})
+}
+
+    module.exports = forecast
